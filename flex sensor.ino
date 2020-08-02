f76b23978f25978ce59060c53498074de5b66b11
Msony55 <52307314+Msony55@users.noreply.github.com>
2020-08-02 14:36:14 +0530

    Add files via upload

---
+++ flex sensor.ino
@@ -0,0 +1,51 @@
+int flexs1 = 0;
+int flexs2 = 1;
+int flexs3 = 2;
+int data1 = 0; 
+int data2 = 0;
+int data3 = 0;
+
+void setup()
+{
+  Serial.begin(9600); 
+  pinMode(flexs1, INPUT);
+  pinMode(flexs2, INPUT);
+  pinMode(flexs3, INPUT);
+}
+
+void loop()
+{  
+   data1 = analogRead(flexs1); 
+   data2 = analogRead(flexs2); 
+   data3 = analogRead(flexs3);
+
+   
+//   Serial.print("value1:");
+//   Serial.println(data1); 
+//   Serial.print("value2:");
+//   Serial.println(data2); 
+//   Serial.print("value3:");
+//   Serial.println(data3);
+  if(Serial.available()>=0)
+  {
+ if( (data1>=109&&data1 <115) && data2>110 && data3<56 ) 
+  {
+    Serial.println("Come Here");
+  }
+
+  if( (data1>=109&&data1 <115) && (data2>110 && data3>=56 ) ) 
+  {
+    Serial.println("Hii");
+  }
+   if( (data1>=102&&data1 <109) && (data2>110 && data3>=56 ) ) 
+  {
+    Serial.println("Washroom");
+  }
+  if( (data1>=102&&data1 <109)  && data3<56 )  
+  {
+    Serial.println("water");
+  }
+ 
+  delay(3000); 
+  } 
+}   
---
+++ home_anti_.ino
@@ -0,0 +1,62 @@
+#include <Servo.h>
+#include <SoftwareSerial.h> 
+
+SoftwareSerial bt(9,10);
+char a,b;
+const int ledpin=13;
+// const int motionpin=2;  
+const int flamePin = 11;
+//int buzzer = 8;
+int servoPin = 13;
+Servo Servo1; 
+void setup() {
+  bt.begin(9600);
+  Servo1.attach(servoPin);
+//  pinMode(buzzer, OUTPUT);
+  pinMode(flamePin, INPUT);
+//   pinMode(motionpin,INPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(8,HIGH);
+  pinMode(3,HIGH);
+}
+void loop() {
+ /* if (digitalRead(2) == HIGH && digitalRead(8) == LOW){             //2 = motion pin
+      digitalWrite(ledpin,HIGH);          // tone(buzzer,100); //turns on led(13) and buzzer
+     digitalWrite(buzzer,HIGH);               // 12 = buzzer
+   // Serial.println('HIGH');
+  }
+   else if (digitalRead(2) == HIGH && digitalRead(8) == HIGH){             //2 = motion pin
+       digitalWrite(ledpin,LOW);          // tone(buzzer,100); //turns on led(13) and buzzer
+       digitalWrite(buzzer,LOW);               // 12 = buzzer
+      // Serial.println('LOW');
+  }
+  if (digitalRead(11) == LOW )                          //flame = 11
+  {
+    digitalWrite(buzzer, HIGH);
+    delay(500);
+    digitalWrite(buzzer, LOW);
+     delay(500);
+  }
+  else if (digitalRead(11) == HIGH )                          //flame = 11
+  {
+    digitalWrite(buzzer, LOW);
+  }*/
+  if(bt.available())
+  {
+    a = bt.read();      //Read the incoming data and store it into variable Incoming_value
+    Serial.println(a);        //Print Value of Incoming_value in Serial monitor
+    if(a == 'A')            //Checks whether value of Incoming_value is equal to a
+       Servo1.write(120);                 //If value is 1 then 90 degree rotate
+    else if(a == 'B')       //Checks whether value of Incoming_value is equal to b
+        Servo1.write(0);                 //If value is 0 then 0 degree rotate
+        if(a == 'C')            //Checks whether value of Incoming_value is equal to a
+        digitalWrite(8,HIGH);              //If value is 1 then 90 degree rotate
+    else if(a == 'D')       //Checks whether value of Incoming_value is equal to b
+        digitalWrite(8,LOW);
+         if(a == 'E')            //Checks whether value of Incoming_value is equal to a
+        digitalWrite(3,HIGH);              //If value is 1 then 90 degree rotate
+    else if(a == 'F')       //Checks whether value of Incoming_value is equal to b
+        digitalWrite(3,LOW); 
+  }
+}
