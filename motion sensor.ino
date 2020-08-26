d2bcae3a139108d6280de85edd162812c7ea4425
Msony55 <52307314+Msony55@users.noreply.github.com>
2020-08-26 12:56:26 +0530

    Add files via upload

---
+++ motion sensor.ino
@@ -0,0 +1,18 @@
+const int PirSensor = 2;
+int motionState = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(PirSensor, INPUT);
+}
+
+void loop() {
+  motionState = digitalRead(PirSensor);
+  if (motionState == HIGH) {
+    Serial.println("1"); 
+    motionState = LOW;
+    delay(20000);
+//    while(0);
+  }
+
+}
